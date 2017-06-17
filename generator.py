c2b49b8e12a461c41b8efe3ff5936adf177b5b8a
Konrad <konrad.lyda@gmail.com>
2017-06-17 13:24:01 +0200

    template testing added temp files and templates folder are set by variables

--- generator.py
+++ generator.py
@@ -3,16 +3,20 @@ from mailmerge import MailMerge
 
 
 class Generator(object):
-    def __init__(self, template_url: str):
-        self.template = template_url
+    temp_files_folder_name = "temp_files"
+    template_folder_name = "templates"
+
+    def __init__(self, template_name: str):
+        self.template = self.template_folder_name + "/" + template_name
         self.document = MailMerge(self.template)
 
     def get_template_fields(self):
         return self.document.get_merge_fields()
 
-    def marge_pages_to_file(self, data, output_file):
+    def marge_pages_to_file(self, data, output_file_name):
         self.document.merge_pages(data)
-        self.document.write(output_file)
+        path = self.temp_files_folder_name + "/" + output_file_name
+        self.document.write(path)
 
 
 
