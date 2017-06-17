98bb35df0d498a8ef16b21ee32bd37de3bea3bdd
Konrad <konrad.lyda@gmail.com>
2017-06-17 18:28:42 +0200

    in test file only sample json left

--- test.py
+++ test.py
@@ -1,67 +1,66 @@
-from generator import Generator
-
-fields = [
-    {
-        'MiastoWydania': 'Warszawa',
-        'NrZRejestru': 'KW001/213/2017',
-        'TerminOd': '16.08.2017',
-        'ImieNazwisko': 'Jan Kowalski',
-        'NrZgody': 'ASDASDASDdf.dfsdfsdf',
-        'DataUr': '16.08.1990',
-        'DataZgody': '01.01.2017',
-        'TerminDo': '18.08.2017',
-        'DataWydania': '18.08.2017',
-        'WydajacyZgode': 'Mazowiecki KO'
-    },
-    {
-        'MiastoWydania': 'Warszawa',
-        'NrZRejestru': 'KW001/213/2017',
-        'TerminOd': '16.08.2017',
-        'ImieNazwisko': 'Marek Kowalski',
-        'NrZgody': 'ASDASDASDdf.dfsdfsdf',
-        'DataUr': '16.08.1990',
-        'DataZgody': '01.01.2017',
-        'TerminDo': '18.08.2017',
-        'DataWydania': '18.08.2017',
-        'WydajacyZgode': 'Mazowiecki KO'
-    },
-    {
-        'MiastoWydania': 'Warszawa',
-        'NrZRejestru': 'KW001/213/2017',
-        'TerminOd': '16.08.2017',
-        'ImieNazwisko': 'John malkovic',
-        'NrZgody': 'ASDASDASDdf.dfsdfsdf',
-        'DataUr': '16.08.1990',
-        'DataZgody': '01.01.2017',
-        'TerminDo': '18.08.2017',
-        'DataWydania': '18.08.2017',
-        'WydajacyZgode': 'Mazowiecki KO'
-    },
-    {
-        'MiastoWydania': 'Warszawa',
-        'NrZRejestru': 'KW001/213/2017',
-        'TerminOd': '16.08.2017',
-        'ImieNazwisko': 'Marek Mostowiak',
-        'NrZgody': 'ASDASDASDdf.dfsdfsdf',
-        'DataUr': '16.08.1990',
-        'DataZgody': '01.01.2017',
-        'TerminDo': '18.08.2017',
-        'DataWydania': '18.08.2017',
-        'WydajacyZgode': 'Mazowiecki KO'
-    },
-    {
-        'MiastoWydania': 'Warszawa',
-        'NrZRejestru': 'KW001/213/2017',
-        'TerminOd': '16.08.2017',
-        'ImieNazwisko': 'Hanna Mostowiak',
-        'NrZgody': 'ASDASDASDdf.dfsdfsdf',
-        'DataUr': '16.08.1990',
-        'DataZgody': '01.01.2017',
-        'TerminDo': '18.08.2017',
-        'DataWydania': '18.08.2017',
-        'WydajacyZgode': 'Mazowiecki KO'
-    }
-]
-
-cert = Generator("cert_kurs_template.docx")
-cert.marge_pages_to_file(fields, "certyfikaty.docx")
+json = {
+    'template': 'cert_kurs_template.docx',
+    'output_file_type': 'docx',
+    'data': [
+        {
+            'MiastoWydania': 'Warszawa',
+            'NrZRejestru': 'KW001/213/2017',
+            'TerminOd': '16.08.2017',
+            'ImieNazwisko': 'Jan Kowalski',
+            'NrZgody': 'ASDASDASDdf.dfsdfsdf',
+            'DataUr': '16.08.1990',
+            'DataZgody': '01.01.2017',
+            'TerminDo': '18.08.2017',
+            'DataWydania': '18.08.2017',
+            'WydajacyZgode': 'Mazowiecki KO'
+        },
+        {
+            'MiastoWydania': 'Warszawa',
+            'NrZRejestru': 'KW001/213/2017',
+            'TerminOd': '16.08.2017',
+            'ImieNazwisko': 'Marek Kowalski',
+            'NrZgody': 'ASDASDASDdf.dfsdfsdf',
+            'DataUr': '16.08.1990',
+            'DataZgody': '01.01.2017',
+            'TerminDo': '18.08.2017',
+            'DataWydania': '18.08.2017',
+            'WydajacyZgode': 'Mazowiecki KO'
+        },
+        {
+            'MiastoWydania': 'Warszawa',
+            'NrZRejestru': 'KW001/213/2017',
+            'TerminOd': '16.08.2017',
+            'ImieNazwisko': 'John malkovic',
+            'NrZgody': 'ASDASDASDdf.dfsdfsdf',
+            'DataUr': '16.08.1990',
+            'DataZgody': '01.01.2017',
+            'TerminDo': '18.08.2017',
+            'DataWydania': '18.08.2017',
+            'WydajacyZgode': 'Mazowiecki KO'
+        },
+        {
+            'MiastoWydania': 'Warszawa',
+            'NrZRejestru': 'KW001/213/2017',
+            'TerminOd': '16.08.2017',
+            'ImieNazwisko': 'Marek Mostowiak',
+            'NrZgody': 'ASDASDASDdf.dfsdfsdf',
+            'DataUr': '16.08.1990',
+            'DataZgody': '01.01.2017',
+            'TerminDo': '18.08.2017',
+            'DataWydania': '18.08.2017',
+            'WydajacyZgode': 'Mazowiecki KO'
+        },
+        {
+            'MiastoWydania': 'Warszawa',
+            'NrZRejestru': 'KW001/213/2017',
+            'TerminOd': '16.08.2017',
+            'ImieNazwisko': 'Hanna Mostowiak',
+            'NrZgody': 'ASDASDASDdf.dfsdfsdf',
+            'DataUr': '16.08.1990',
+            'DataZgody': '01.01.2017',
+            'TerminDo': '18.08.2017',
+            'DataWydania': '18.08.2017',
+            'WydajacyZgode': 'Mazowiecki KO'
+        }
+    ]
+}
